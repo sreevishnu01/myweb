a05288c30c8c4e407be5213173b2bf76558786a2
sreevishnu01 <sreevishnu01@gmail.com>
2021-12-25 18:01:35 +0400

    title change hashrouter and github page depolye added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import Home from './components/Home';
 import MainCards from './components/layout/cards/MainCards';
 import Blog from './components/myBlog/Blog';
